6e43af82820dd0280f1b73521a62f656c88e9358
junior-r <JUNIOR31064049@GMAIL.com>
2021-05-24 13:32:07 -0500

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,16 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import jquery from 'jquery'
+window.$ = window.jquery = jquery
+require ("semantic-ui-sass")
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+})
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
